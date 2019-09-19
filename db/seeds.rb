7f34446c5db5c4f551fa3534198bcba89f392d3f
App Academy Student <student@appacademy.io>
2019-09-19 12:01:10 -0700

    update seed file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,10 @@
 
 require "open-uri"
 
+
+ActiveRecord::Base.transaction do 
+
+
   User.destroy_all
   Artist.destroy_all
   Album.destroy_all 
@@ -48,113 +52,114 @@ require "open-uri"
 
 
 
-  follow1 = Follow.create(user_id: 2, artist_id: 4)
-  follow2 = Follow.create(user_id: 1, artist_id: 2)
-  follow3 = Follow.create(user_id: 1, artist_id: 3)
-  follow4 = Follow.create(user_id: 3, artist_id: 5)
-  follow5 = Follow.create(user_id: 3, artist_id: 7)
+  follow1 = Follow.create(user_id: user2.id, artist_id: artist1.id)
+  follow2 = Follow.create(user_id: user1.id, artist_id: artist2.id)
+  follow3 = Follow.create(user_id: user1.id, artist_id: artist3.id)
+  follow4 = Follow.create(user_id: user3.id, artist_id: artist5.id)
+  follow5 = Follow.create(user_id: user3.id, artist_id: artist7.id)
 
-  album1 = Album.create(name: 'The Marshall Mathers LP', artist_id: 1)
-  album2 = Album.create(name: 'Revival', artist_id: 1)
-
-
-
-    song1 = Song.create(title: 'Stan (feat. Dido)', artist_id: 1, album_id: 1, length: 330)
-    song2 = Song.create(title: 'The Real Slim Shady', artist_id: 1, album_id: 1, length: 330)
-    song3 = Song.create(title: 'Who Knew', artist_id: 1, album_id: 1, length: 330)
-
-    song4 = Song.create(title: 'Walk On Water (feat. Beyonce)', artist_id: 1, album_id: 2, length: 330)
-    song5 = Song.create(title: 'Believe', artist_id: 1, album_id: 2, length: 330)
-    song6 = Song.create(title: 'River', artist_id: 1, album_id: 2, length: 330)
-    song7 = Song.create(title: 'Untouchable', artist_id: 1, album_id: 2, length: 330)
-
-
-  album3 = Album.create(name: 'This Is Acting', artist_id: 2)
-  album4 = Album.create(name: 'We Are Born', artist_id: 2)
-
-
-    song8 = Song.create(title: 'Bird Set Free', artist_id: 2, album_id: 3, length: 330)
-    song9 = Song.create(title: 'Alive', artist_id: 2, album_id: 3, length: 330)
-    song10 = Song.create(title: 'Unstoppable', artist_id: 2, album_id: 3, length: 330)
-    song11 = Song.create(title: 'House On Fire', artist_id: 2, album_id: 3, length: 330)
-
-    song12 = Song.create(title: 'Clap Your Hands', artist_id: 2, album_id: 4, length: 330)
-    song13 = Song.create(title: 'Be Good To Me', artist_id: 2, album_id: 4, length: 330)
-    song14 = Song.create(title: 'The Fight', artist_id: 2, album_id: 4, length: 330)
-    song15 = Song.create(title: 'Stop Trying', artist_id: 2, album_id: 4, length: 330)
-
-  album5 = Album.create(name: 'Noir', artist_id: 3)
-  album6 = Album.create(name: 'Blkswn', artist_id: 3)
-
-    song16 = Song.create(title: 'KLINK', artist_id: 3, album_id: 5, length: 330)
-    song17 = Song.create(title: 'TEQUILA MOCKINGBIRD', artist_id: 3, album_id: 5, length: 330)
-    song18 = Song.create(title: 'MERLOT', artist_id: 3, album_id: 5, length: 330)
-
-    song19 = Song.create(title: 'Maraca', artist_id: 3, album_id: 6, length: 330)
-    song20 = Song.create(title: 'blkswn', artist_id: 3, album_id: 6, length: 330)
-    song21 = Song.create(title: 'Long Run', artist_id: 3, album_id: 6, length: 330)
-    song22 = Song.create(title: 'Anita', artist_id: 3, album_id: 6, length: 330)
-
-
-  album7 = Album.create(name: 'Loud', artist_id: 4)
-  album8 = Album.create(name: 'Unapologetic', artist_id: 4)
-
-  album9 = Album.create(name: 'Pearl', artist_id: 5)
-  album10 = Album.create(name: 'In Concert', artist_id: 5)
-
-  album11 = Album.create(name: 'Lemonade', artist_id: 6)
-  album12 = Album.create(name: 'The Lion King: The Gift', artist_id: 6)
+  album1 = Album.create(name: 'The Marshall Mathers LP', artist_id: artist1.id)
+  album2 = Album.create(name: 'Revival', artist_id: artist1.id)
 
 
 
-  playlist1 = Playlist.create(name: 'Workout Music', user_id: 2)
-  playlist2 = Playlist.create(name: 'Favorite Music', user_id: 1)
-  playlist3 = Playlist.create(name: 'My number 1 playlist', user_id: 4)
-  playlist4 = Playlist.create(name: 'My Morning playlist', user_id: 4)
-  playlist5 = Playlist.create(name: 'My Evening playlist', user_id: 1)
-  playlist6 = Playlist.create(name: 'Relaxing songs', user_id: 3)
+  song1 = Song.create(title: 'Stan (feat. Dido)', artist_id: artist1.id, album_id: album1.id, length: 330)
+  # song1.picture.attach(io: ) 
+
+  song2 = Song.create(title: 'The Real Slim Shady', artist_id: artist1.id, album_id: album1.id, length: 330)
+  song3 = Song.create(title: 'Who Knew', artist_id: artist1.id, album_id: album1.id, length: 330)
+
+  song4 = Song.create(title: 'Walk On Water (feat. Beyonce)', artist_id: artist1.id, album_id: album2.id, length: 330)
+  song5 = Song.create(title: 'Believe', artist_id: artist1.id, album_id: album2.id, length: 330)
+  song6 = Song.create(title: 'River', artist_id: artist1.id, album_id: album2.id, length: 330)
+  song7 = Song.create(title: 'Untouchable', artist_id: artist1.id, album_id: album2.id, length: 330)
+
+
+  album3 = Album.create(name: 'This Is Acting', artist_id: artist2.id)
+  album4 = Album.create(name: 'We Are Born', artist_id: artist2.id)
+
+
+  song8 = Song.create(title: 'Bird Set Free', artist_id: artist2.id, album_id: album3.id, length: 330)
+  song9 = Song.create(title: 'Alive', artist_id: artist2.id, album_id: album3.id, length: 330)
+  song10 = Song.create(title: 'Unstoppable', artist_id: artist2.id, album_id: album3.id, length: 330)
+  song11 = Song.create(title: 'House On Fire', artist_id: artist2.id, album_id: album3.id, length: 330)
+  song12 = Song.create(title: 'Clap Your Hands', artist_id: artist2.id, album_id: album4.id, length: 330)
+  song13 = Song.create(title: 'Be Good To Me', artist_id: artist2.id, album_id: album4.id, length: 330)
+  song14 = Song.create(title: 'The Fight', artist_id: artist2.id, album_id: album4.id, length: 330)
+  song15 = Song.create(title: 'Stop Trying', artist_id: artist2.id, album_id: album4.id, length: 330)
+
+  album5 = Album.create(name: 'Noir', artist_id: artist3.id)
+  album6 = Album.create(name: 'Blkswn', artist_id: artist3.id)
+
+  song16 = Song.create(title: 'KLINK', artist_id: artist3.id, album_id: album5.id, length: 330)
+  song17 = Song.create(title: 'TEQUILA MOCKINGBIRD', artist_id: 3, album_id: album5.id, length: 330)
+  song18 = Song.create(title: 'MERLOT', artist_id: artist3.id, album_id: album5.id, length: 330)
+
+  song19 = Song.create(title: 'Maraca', artist_id: artist3.id, album_id: album6.id, length: 330)
+  song20 = Song.create(title: 'blkswn', artist_id: artist3.id, album_id: album6.id, length: 330)
+  song21 = Song.create(title: 'Long Run', artist_id: artist3.id, album_id: album6.id, length: 330)
+  song22 = Song.create(title: 'Anita', artist_id: artist3.id, album_id: album6.id, length: 330)
+
+
+  album7 = Album.create(name: 'Loud', artist_id: artist4.id)
+  album8 = Album.create(name: 'Unapologetic', artist_id: artist4.id)
+
+  album9 = Album.create(name: 'Pearl', artist_id: artist5.id)
+  album10 = Album.create(name: 'In Concert', artist_id: artist5.id)
+
+  album11 = Album.create(name: 'Lemonade', artist_id: artist6.id)
+  album12 = Album.create(name: 'The Lion King: The Gift', artist_id: artist6.id)
+
+
+
+  playlist1 = Playlist.create(name: 'Workout Music', user_id: user2.id)
+  playlist2 = Playlist.create(name: 'Favorite Music', user_id: user1.id)
+  playlist3 = Playlist.create(name: 'My number 1 playlist', user_id: user3.id)
+  playlist4 = Playlist.create(name: 'My Morning playlist', user_id: user4.id)
+  playlist5 = Playlist.create(name: 'My Evening playlist', user_id: user1.id)
+  playlist6 = Playlist.create(name: 'Relaxing songs', user_id: user3.id)
   
 
 
-  playlistsong1 = Playlistsong.create(playlist_id: 1, song_id: 2, ord: 1)
-  playlistsong2 = Playlistsong.create(playlist_id: 1, song_id: 4, ord: 2)
-  playlistsong3 = Playlistsong.create(playlist_id: 1, song_id: 5, ord: 3)
-  playlistsong4 = Playlistsong.create(playlist_id: 1, song_id: 3, ord: 4)
-  playlistsong5 = Playlistsong.create(playlist_id: 1, song_id: 6, ord: 5)
-  playlistsong6 = Playlistsong.create(playlist_id: 1, song_id: 7, ord: 6)
+  playlistsong1 = Playlistsong.create(playlist_id: playlist1.id, song_id: song2.id, ord: 1)
+  playlistsong2 = Playlistsong.create(playlist_id: playlist1.id, song_id: song4.id, ord: 2)
+  playlistsong3 = Playlistsong.create(playlist_id: playlist1.id, song_id: song5.id, ord: 3)
+  playlistsong4 = Playlistsong.create(playlist_id: playlist1.id, song_id: song3.id, ord: 4)
+  playlistsong5 = Playlistsong.create(playlist_id: playlist1.id, song_id: song6.id, ord: 5)
+  playlistsong6 = Playlistsong.create(playlist_id: playlist1.id, song_id: song7.id, ord: 6)
+
+  playlistsong7 = Playlistsong.create(playlist_id: playlist2.id, song_id: song1.id, ord: 1)
+  playlistsong8 = Playlistsong.create(playlist_id: playlist2.id, song_id: song4.id, ord: 2)
+  playlistsong9 = Playlistsong.create(playlist_id: playlist2.id, song_id: song1.id, ord: 3)
+  playlistsong10 = Playlistsong.create(playlist_id: playlist2.id, song_id: song1.id, ord: 4)
+  playlistsong11 = Playlistsong.create(playlist_id: playlist2.id, song_id: song9.id, ord: 5)
+
+  playlistsong12 = Playlistsong.create(playlist_id: playlist3.id, song_id: song1.id, ord: 1)
+  playlistsong13 = Playlistsong.create(playlist_id: playlist3.id, song_id: song1.id, ord: 2)
+  playlistsong14 = Playlistsong.create(playlist_id: playlist3.id, song_id: song1.id, ord: 3)
 
 
-  playlistsong7 = Playlistsong.create(playlist_id: 2, song_id: 1, ord: 1)
-  playlistsong8 = Playlistsong.create(playlist_id: 2, song_id: 4, ord: 2)
-  playlistsong9 = Playlistsong.create(playlist_id: 2, song_id: 11, ord: 3)
-  playlistsong10 = Playlistsong.create(playlist_id: 2, song_id: 14, ord: 4)
-  playlistsong11 = Playlistsong.create(playlist_id: 2, song_id: 9, ord: 5)
-
-  playlistsong12 = Playlistsong.create(playlist_id: 3, song_id: 10, ord: 1)
-  playlistsong13 = Playlistsong.create(playlist_id: 3, song_id: 14, ord: 2)
-  playlistsong14 = Playlistsong.create(playlist_id: 3, song_id: 17, ord: 3)
+  playlistsong15 = Playlistsong.create(playlist_id: playlist4.id, song_id: song15.id, ord: 1)
+  playlistsong16 = Playlistsong.create(playlist_id: playlist4.id, song_id: song5.id, ord: 2)
+  playlistsong17 = Playlistsong.create(playlist_id: playlist4.id, song_id: song20.id, ord: 3)
 
 
-  playlistsong15 = Playlistsong.create(playlist_id: 4, song_id: 15, ord: 1)
-  playlistsong16 = Playlistsong.create(playlist_id: 4, song_id: 5, ord: 2)
-  playlistsong17 = Playlistsong.create(playlist_id: 4, song_id: 20, ord: 3)
+  playlistsong18 = Playlistsong.create(playlist_id: playlist5.id, song_id: song16.id, ord: 1)
+  playlistsong19 = Playlistsong.create(playlist_id: playlist5.id, song_id: song17.id, ord: 2)
+  playlistsong20 = Playlistsong.create(playlist_id: playlist5.id, song_id: song4.id, ord: 3)
 
-
-  playlistsong18 = Playlistsong.create(playlist_id: 5, song_id: 16, ord: 1)
-  playlistsong19 = Playlistsong.create(playlist_id: 5, song_id: 17, ord: 2)
-  playlistsong20 = Playlistsong.create(playlist_id: 5, song_id: 4, ord: 3)
-
-  playlistsong21 = Playlistsong.create(playlist_id: 6, song_id: 14, ord: 1)
-  playlistsong22 = Playlistsong.create(playlist_id: 6, song_id: 19, ord: 2)
-  playlistsong23 = Playlistsong.create(playlist_id: 6, song_id: 3, ord: 3)
+  playlistsong21 = Playlistsong.create(playlist_id: playlist6.id, song_id: song14.id, ord: 1)
+  playlistsong22 = Playlistsong.create(playlist_id: playlist6.id, song_id: song19.id, ord: 2)
+  playlistsong23 = Playlistsong.create(playlist_id: playlist6.id, song_id: song3.id, ord: 3)
 
 
 
 #  Attaching Photos to each artist 
 
+  j = 0
   Artist.all.each do |artist|
     
-    id = artist.id 
+    id = j + 1 
 
     photofile = open("https://craftifybucket.s3.us-east-2.amazonaws.com/#{id}.jpg")
 
@@ -162,41 +167,33 @@ require "open-uri"
       io: photofile,
       filename: "#{id}.jpg"
     )
+
+    j += 1
     end
 
+  
+  
+    i = 0
 
     Song.all.each do |song|
-    
-      id = song.id 
+        
+      id = i + 1
 
       filesong = open("https://craftifybucket.s3.us-east-2.amazonaws.com/#{id}.mp3")
 
       song.mp3song.attach(
         io: filesong,
         filename: "#{id}.jpg"
-       )
-    end
+      )
+
+      i += 1
+
+     end
+
+
+  end  
 
 
 
 
-  #  eminem = Artist.first
-
-  #   id = eminem.id 
-  #   photofile = open("https://craftifybucket.s3.us-east-2.amazonaws.com/1.jpg")
-        
-  #   eminem.picture.attach(
-  #     io: photofile,
-  #     filename: "1.jpg"
-  #   )
-
-
-
-  #   sia = Artist.second
-    
-  #   photofile2 = open("https://craftifybucket.s3.us-east-2.amazonaws.com/2.jpg")
-        
-  #   sia.picture.attach(
-  #     io: photofile2,
-  #     filename: "2.jpg"
-  #   )
+ 
